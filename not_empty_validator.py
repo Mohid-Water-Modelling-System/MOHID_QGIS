bf30d95801f10d26690811bfc3b00f0d597b4468
vascoguita <vasco.guita@tecnico.ulisboa.pt>
2021-10-30 14:52:20 +0200

    Refactor

--- not_empty_validator.py
+++ not_empty_validator.py
@@ -1,9 +1,7 @@
 from qgis.PyQt.QtGui import QValidator
-from qgis.core import Qgis, QgsMessageLog
 
 class NotEmptyValidator(QValidator):
     def validate(self, text, pos):
-        QgsMessageLog.logMessage("NotEmptyValidator: validating", 'MOHID plugin', level=Qgis.Info)
         if text.strip():
             state = QValidator.Acceptable
         else:
